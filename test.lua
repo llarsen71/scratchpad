87fe4c6acd7fb4319ba506bb40d669c7c838b766
Lance Larsen <gaffer.lc@gmail.com>
2013-12-16 19:25:49 -0700

    Start

---
+++ test.lua
@@ -0,0 +1,55 @@
+function rest(poke, length)
+  poke.powerpoints = poke.powerpoints + length/2  
+end
+
+function pp(poke)
+  print("----------------------------------------")
+  print("Name:        " .. poke.name)
+  print("Type:")
+  for i = 1, #poke.type do
+    print('    ' .. poke.type[i])
+  end
+  print("powerpoints: " .. poke.powerpoints)
+  print("health: " .. poke.health)
+  if poke.health < 1 then
+    poke.status = "knocked out"
+  end
+  print("status:  " .. poke.status)
+  print('number of attacks : ' .. #poke.attack)
+  for i = 1, #poke.attack do
+    print("   " .. poke.attack[i])
+  end
+  print('----------------------------------------')
+end
+
+pokemon  = {type={"steel","ground"}, 
+            powerpoints=5, 
+            health=100,
+            status="normal",
+            name = "Steelix",
+            attack={'tail whip','dig'}, 
+            printme=pp, 
+            rest=rest}
+pokemon2 = {type={"water"}, 
+            powerpoints = 3, 
+            health=100,
+            status="normal",
+            name = "Piplup",
+            attack={'bubble'}, 
+            printme=pp, 
+            rest=rest}
+pokemon3 = {type={"electric"}, 
+            powerpoints = 11, 
+            health=0,
+            status="normal",
+            name = "Pikachu",
+            attack={'thunder bolt','tackle'}, 
+            printme=pp, 
+            rest=rest} 
+
+pokemon:rest(10)
+pokemon:printme()
+pokemon2:rest(20)
+pokemon2:printme()
+pokemon3:rest(30)
+pokemon3:printme()
